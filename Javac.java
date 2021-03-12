1108eb612df944e57dff00262a2c9187502f6a6d
Itzbenz <gamerrindoubuntu@gmail.com>
2021-03-12 18:31:33 +0100

    yeet

--- Javac.java
+++ Javac.java
@@ -6,7 +6,7 @@ import java.nio.file.StandardOpenOption;
 public class Javac {
 	
 	static String head = "import java.io.*;\n" + "import java.util.*;\n" + "public class Src {\n" + "\tpublic static void main(String[] args) {", foot = "\t}\n" + "}\n";
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		StringBuilder code = new StringBuilder(head);
 		code.append(String.join(" ", args));
 		code.append(foot);
@@ -14,8 +14,16 @@ public class Javac {
 		temp.delete();
 		Files.writeString(temp.toPath(), code, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
 		temp.deleteOnExit();
-		System.out.println("javac: " + (Runtime.getRuntime().exec("javac").exitValue() == 0 ? "Exists" : "Nope"));
-		System.out.println("javac: " + new ProcessBuilder("javac", temp.getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().exitValue());
-		System.out.println("java: " + new ProcessBuilder("java", new File(temp.getParent(), "Src").getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().exitValue());
+		boolean javac = false;
+		try{
+			javac = Runtime.getRuntime().exec("javac --version").waitFor() == 0;
+		}catch (IOException ignored){}
+		System.out.println("javac: " + (javac ? "Exists" : "Nope"));
+		if(!javac){
+			System.out.println("Don't have javac");
+			return;
+		}
+		System.out.println("javac: " + new ProcessBuilder("javac", temp.getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().waitFor());
+		System.out.println("java: " + new ProcessBuilder("java", new File(temp.getParent(), "Src").getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().waitFor());
 	}
 }
