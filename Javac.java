6cf6c4895a827f98ba1cd425a17ca02200be743f
Itzbenz <gamerrindoubuntu@gmail.com>
2021-03-12 18:09:34 +0100

    h

---
+++ Javac.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+public class Javac {
+	
+	static String head = "import java.io.*;\n" + "import java.util.*;\n" + "public class Src {\n" + "\tpublic static void main(String[] args) {", foot = "\t}\n" + "}\n";
+	public static void main(String[] args) throws IOException {
+		StringBuilder code = new StringBuilder(head);
+		code.append(String.join(" ", args));
+		code.append(foot);
+		File temp = new File(System.getProperty("java.io.tmpdir"), "Src.java");
+		temp.delete();
+		Files.writeString(temp.toPath(), code, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
+		temp.deleteOnExit();
+		System.out.println("javac: " + new ProcessBuilder("javac " +temp.getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().exitValue());
+		System.out.println("java: " + new ProcessBuilder("java " + new File(temp.getParent(), "Src").getAbsolutePath()).redirectError(ProcessBuilder.Redirect.PIPE).redirectOutput(ProcessBuilder.Redirect.PIPE).start().exitValue());
+	}
+}
