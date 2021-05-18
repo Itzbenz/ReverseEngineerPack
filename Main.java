42ba93a30071bb24e0f79cd9a544f3058967d0a7
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-05-18 09:48:48 +0200

    bruh

--- Main.java
+++ Main.java
@@ -79,7 +79,7 @@ public class Main {
             ArrayList<URL> ping = new ArrayList<>();
             for (String ss : s.split(" ")) {
                 try {
-                    ss = (ss.endsWith("/") ? "" : "/") + "assad";
+                    ss = ss + (ss.endsWith("/") ? "" : "/") + "assad";
                     ping.add(new URL(ss));
                     System.out.println("Registered: " + ss);
                 }catch(MalformedURLException e){
